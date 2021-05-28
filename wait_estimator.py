275813e45d78ba2fb2989db57db1637f7b0172eb
Diva Kartika <79572421+divakartika@users.noreply.github.com>
2021-05-29 00:33:24 +0700

    Save model of wait_esimator.py
    This is by far our best model
    
    Data: WaitDataF1 (no Arrival Time)
    Dense Layer: 16 64 64
    optimizer=adam
    epoch=200
    
    MAE = 4.448918172235603
    RMSE = 8.167037667027486
    EVS = 0.5664157877001628
    
    Co-Authored-By: Ja'far Hilmy Farhan <68992403+jafarhf1@users.noreply.github.com>

--- wait_estimator.py
+++ wait_estimator.py
@@ -8,103 +8,64 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import mean_squared_error,mean_absolute_error,explained_variance_score
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+# from sklearn.preprocessing import MinMaxScaler
 
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import regularizers
 from tensorflow.keras.layers.experimental import preprocessing
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, LSTM, Dropout, Flatten, Dense
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.layers import LSTM, Dropout, Flatten, Dense
+# from tensorflow.keras.callbacks import EarlyStopping
 
 print(tf.__version__)
 
 #%%
 
-
 df1 = pd.read_csv('WaitDataF1.csv')
 df2 = pd.read_csv('WaitDataF2.csv')
 df3 = pd.read_csv('WaitDataF3.csv')
 df = pd.concat([df1,df2,df3])
-df.loc[df["Wait"] < 0, "Wait"] = 0
-#df.drop(df[df['Wait'] <= -180].index, inplace=True)
-df.drop(df[df['Wait'] >= 180].index, inplace=True)
-#df = df.sort_index(axis=0,ascending=False,ignore_index=True)
+df1.loc[df1["Wait"] < 0, "Wait"] = 0
 
-X = df.drop('Wait',
-            axis=1)
-y = df['Wait']
+X = df1.drop(['Wait','ThoracicCount','PediatricCount','NeuroCount','CardiacCount'],axis=1)
+y = df1['Wait']
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=1/10,random_state=len(X))
 X_train, X_val, y_train, y_val = train_test_split(X_train,y_train,test_size=1/9,random_state=len(X_train))
 
 #%%
+
 normalizer = preprocessing.Normalization()
 normalizer.adapt(np.array(X_train))
 
 #%%
-#scaler = MinMaxScaler()
-#X_train= scaler.fit_transform(X_train)
-#X_test = scaler.transform(X_test)
 
-#%%
 model = keras.Sequential([
     normalizer,
-    #tf.keras.layers.Conv1D(filters=32, kernel_size=3,
-                      #strides=1, padding="causal",
-                      #activation="relu",
-                      #input_shape=[None, 1]
-                      #),
-    #LSTM(64, return_sequences=True),
-  #tf.keras.layers.LSTM(64, return_sequences=True),
-    # Dense(16, activation='relu', 
-           # kernel_regularizer=regularizers.l2(0.001)
-           # ),
-    Dense(16, activation='relu', 
-          kernel_regularizer=regularizers.l2(0.001)
+    Dense(16, activation='relu'
           ),
-    #Dropout(0.1),
-    Dense(32, activation='relu', 
-          kernel_regularizer=regularizers.l2(0.001)
+    Dense(64, activation='relu'
           ),
-    #Dropout(0.1),
-    Dense(64, activation='relu', 
-          kernel_regularizer=regularizers.l2(0.001)
+    Dense(64, activation='relu'
           ),
-    #Dropout(0.1),
-    Dense(64, activation='relu', 
-          kernel_regularizer=regularizers.l2(0.001)
-          ),
-    #Dropout(0.1),
-    # Dense(64, activation='relu', 
-           # kernel_regularizer=regularizers.l2(0.001)
-           # ),
-    #Dropout(0.1),
-    #Dense(512, activation='relu', 
-          #kernel_regularizer=regularizers.l2(0.001)
-     #      ),
-    #Dropout(0.1),
-    #Dense(512, activation='relu'),
-    #Dropout(0.1),
     Dense(1, activation='relu')
     ])
+
 #%%
+
 model.compile(optimizer='adam', 
-              loss='mae',
-              #metrics=['accuracy']
+              loss='mae'
               )
 
 model.summary()
-#%%
 
-#early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=10)
+#%%
 
 history = model.fit(x=X_train,
                     y=y_train.values,
                     validation_data=(X_val,y_val.values),
-                    batch_size=655,
-                    epochs=600
-                    #callbacks=[early_stop]
+                    batch_size=342,
+                    epochs=200
                     )
 
 history
@@ -115,7 +76,7 @@ losses.plot()
 
 #%%
 
-#PREDICT WITH TEST SET
+
 predictions = model.predict(X_val)
 mae = mean_absolute_error(y_val,predictions)
 
@@ -128,15 +89,19 @@ evs = explained_variance_score(y_val,predictions)
 print('EVS =',evs)
 
 predictions = model.predict(X_val.head(5))
-#mae = mean_absolute_error(y_test[0],predictions)
-
-#rmse = np.sqrt(mean_squared_error(y_test[0],predictions))
 
 print('prediction =',predictions)
 print('label =', y_val.values[:5])
-#print('MAE =', mae)
-#print('RMSE =', rmse)
+
+#PREDICT WITH TEST SET
+predictions = model.predict(X_test.head(5))
+
+print('test set prediction =',predictions)
+print('test set label =', y_test.values[:5])
 
 #%%
 
-#model.save('wait_est_model.h5')
+model.save('wait_est_model.h5')
+
+#%%
+model.save('saved_model/my_model')
